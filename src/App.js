bdab88f8afc9cb6cbabc3c9582ff00cba8a1928c
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-25 14:46:29 +0900

    자식 -> 부모 -> 또다른 자식 상태 전달
    부모 컴포넌트(App)의 state를 자식 컴포넌트(PaymentForm)에 전달

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import PaymentForm from "./components/PaymentForm/PaymentForm";
 import Expenses from "./components/Payments/Expenses";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: "e1",
       title: "수건",
       amount: 12.33,
       date: new Date(2025, 8, 14),
     },
-  ];
+  ]);
 
-  // 이 함수컴포넌트를 자식 컴포넌트에서 실행시키기
   const getPaymentFormData = (data) => {
     console.log(data);
+    setExpenses([
+      {
+        id: Math.random().toString(),
+        title: data.name,
+        amount: data.price,
+        // 강의에서는 이렇게 작성됨
+        // => ExpenseDate 컴포넌트 파일에서 오류남
+        // date: data.today,
+        // 챗 GPT 검색결과 date 객체가 아니라서 나는 오류일 가능성
+        // => 콘솔로 찍어본 결과 console.log(typeof data.today)는 String 타입
+        // => state를 정의할때 date를 날짜객체로 받았는데 data.today가 문자열이라서 나는 오류
+        date: new Date(data.today),
+      },
+    ]);
   };
 
   return (
--- src/components/PaymentForm/PaymentForm.js
+++ src/components/PaymentForm/PaymentForm.js
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 
 import "./PaymentForm.css";
 
-// PaymentForm의 부모컴포넌트(App 컴포넌트)에서 콘솔 찍고
-// form data 받기
-// 부모컴포넌트의 함수를 props로 받을수있음
-// => 자식컴포넌트는 부모컴포넌트에 props로 보낼 수 없음!!
-// 중괄호 붙여서 작성해야함!! => 함수정의 안됨
 const PaymentForm = ({ getPaymentFormData }) => {
   const [objectState, setObjectState] = useState({
     name: "",
@@ -37,10 +32,7 @@ const PaymentForm = ({ getPaymentFormData }) => {
   const buttonSubmitHander = (event) => {
     event.preventDefault();
 
-    // getPaymentFormData 함수를 만들때 값을 바로 콘솔로 찍을수있게 만들었기 때문에
-    // 이렇게 작성하면 console.log(objectState)와 같은 뜻
     getPaymentFormData(objectState);
-    // console.log(objectState);
 
     setObjectState({
       name: "",
