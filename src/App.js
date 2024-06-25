2756b9d878f4f2a3f04b041cb03d1ea4be4d29ee
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-25 16:25:06 +0900

    리스트 추가하기
    32강 리스트 렌더링 -1- 리스트 렌더링
    
    - 스프레드 연산자 사용

--- src/App.js
+++ src/App.js
@@ -4,33 +4,43 @@ import PaymentForm from "./components/PaymentForm/PaymentForm";
 import Expenses from "./components/Payments/Expenses";
 
 function App() {
-	const [expenses, setExpenses] = useState([
-		{
-			id: "e1",
-			title: "수건",
-			amount: 12.33,
-			date: new Date(2025, 8, 14),
-		},
-	]);
+  const [expenses, setExpenses] = useState([
+    {
+      id: "e1",
+      title: "수건",
+      amount: 12.33,
+      date: new Date(2025, 8, 14),
+    },
+  ]);
 
-	const getPaymentFormData = (data) => {
-		console.log(data);
-		setExpenses([
-			{
-				id: Math.random().toString(),
-				title: data.name,
-				amount: data.price,
-				date: new Date(data.today),
-			},
-		]);
-	};
+  const getPaymentFormData = (data) => {
+    // 새로 입력한 값
+    console.log("data :", data);
+    // 기존에 있던 값
+    console.log("expenses :", expenses);
 
-	return (
-		<>
-			<PaymentForm getPaymentFormData={getPaymentFormData} />
-			<Expenses items={expenses} />
-		</>
-	);
+    // 새로 입력한 값이 배열의 맨 처음이 되고
+    // 기존에 있던 값은 하나씩 뒤로 밀리면서
+    // 배열의 개수가 늘어나고, 인덱스값도 하나씩 밀려야 함
+    setExpenses([
+      {
+        id: Math.random().toString(),
+        title: data.name,
+        amount: data.price,
+        date: new Date(data.today),
+      },
+      // state값 추가하기
+      // expenses는 배열인데 앞으로 갯수가 얼마나 늘어날지 모르기때문에 스프레드 연산자로 넣어주면됨
+      ...expenses,
+    ]);
+  };
+
+  return (
+    <>
+      <PaymentForm getPaymentFormData={getPaymentFormData} />
+      <Expenses items={expenses} />
+    </>
+  );
 }
 
 export default App;
