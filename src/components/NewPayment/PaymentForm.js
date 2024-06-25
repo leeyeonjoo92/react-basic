ecff4cdf65674a2564a7cb2e1c91cdd7787b5694
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-25 12:31:17 +0900

    state 객체로 만들어서 관리
    28강 리액트 상태 -4- Form Input

--- src/components/NewPayment/PaymentForm.js
+++ src/components/NewPayment/PaymentForm.js
@@ -5,29 +5,36 @@ import "./PaymentForm.css";
 // input에서 값을 받아서 콘솔에 찍기
 const PaymentForm = () => {
   // 상태는 무조건 컴포넌트 안에서 만들어야됨
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState(0);
-  const [today, setToday] = useState(null);
-  // console.log("name", name);
-  // console.log("price", price);
-  // console.log("today", today);
+  const [objectState, setObjectState] = useState({
+    name: "",
+    price: 0,
+    today: null,
+  });
 
   // 이벤트 핸들러
   // event : 발생하는 이벤트와 관련된 모든값들이 들어가있음
   // event.target.value : 값을 찍을때마다 값을 받아옴
   const inputTypeTextChangeHandler = (event) => {
-    setName(event.target.value);
+    // 이전 상태를 prevState 매개변수로 받아서
+    // name은 바뀐값으로 업데이트하고 나머지는 이전값을 그대로 가지고있음
+    setObjectState((prevState) => ({ ...prevState, name: event.target.value }));
   };
   const inputTypeNumberChangeHandler = (event) => {
-    setPrice(event.target.value);
+    setObjectState((prevState) => ({
+      ...prevState,
+      price: event.target.value,
+    }));
   };
   const inputTypeDateChangeHandler = (event) => {
-    setToday(event.target.value);
+    setObjectState((prevState) => ({
+      ...prevState,
+      today: event.target.value,
+    }));
   };
   const buttonSubmitHandler = () => {
-    console.log("name", name);
-    console.log("price", price);
-    console.log("today", today);
+    console.log("name", objectState.name);
+    console.log("price", objectState.price);
+    console.log("today", objectState.today);
   };
 
   return (
@@ -39,7 +46,7 @@ const PaymentForm = () => {
           {/* onChange : input의 값이 바뀌는 이벤트가 발생하면 핸들러 실행 */}
           <input
             type="text"
-            value={name}
+            value={objectState.name}
             onChange={inputTypeTextChangeHandler}
           />
         </div>
@@ -49,7 +56,7 @@ const PaymentForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-            value={price}
+            value={objectState.price}
             onChange={inputTypeNumberChangeHandler}
           />
         </div>
@@ -59,7 +66,7 @@ const PaymentForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
-            value={today}
+            value={objectState.today}
             onChange={inputTypeDateChangeHandler}
           />
         </div>
