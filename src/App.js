724359092fd51da809d30ddd1fe1ca4977543722
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-25 15:16:24 +0900

    레포지토리 클론
    3-7

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import NewPayment from './components/NewPayment/NewPayment';
 
@@ -5,6 +6,13 @@ function App() {
 
   return (
     <NewPayment/>
+=======
+function App() {
+  return (
+    <div>
+      <p>hello image</p>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
