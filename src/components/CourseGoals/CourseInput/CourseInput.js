cce9fda2c3136f765c4c615a91adeb76e535741c
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-26 13:37:51 +0900

    동적인 인라인 스타일
    35강 스타일링 -1- 동적인 스타일링

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,17 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  // input에 값이 입력될때마다 실행되는 함수
+  const goalInputChangeHandler = (event) => {
+    // 값을 입력할때마다 isValid가 true여야함
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    // 빈값인 경우 빨간색 배경
+    // enteredValue === "" 라고 작성할 경우 "     " 이런식이어도 값이 입력됐다고 인식함
+    // trim() : 모든 공백을 제외한 문자열
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+			setEnteredValue("");
+      // return 해주지 않으면 아래 로직까지 통과해버림
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
@@ -19,7 +34,14 @@ const CourseInput = props => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>목표</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            backgroundColor: isValid ? "transparent" : "salmon",
+            borderColor: isValid ? "#CCC" : "red",
+          }}
+        />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
