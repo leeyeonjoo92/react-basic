efe98d27a98404bde0ea827c7e5c33f7b25d5555
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-06-26 13:47:55 +0900

    css 클래스를 동적으로 조작하기
    35강 스타일링 -1- 동적인 스타일링

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -7,9 +7,7 @@ const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  // input에 값이 입력될때마다 실행되는 함수
   const goalInputChangeHandler = (event) => {
-    // 값을 입력할때마다 isValid가 true여야함
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
@@ -18,13 +16,9 @@ const CourseInput = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    // 빈값인 경우 빨간색 배경
-    // enteredValue === "" 라고 작성할 경우 "     " 이런식이어도 값이 입력됐다고 인식함
-    // trim() : 모든 공백을 제외한 문자열
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
-			setEnteredValue("");
-      // return 해주지 않으면 아래 로직까지 통과해버림
+      setEnteredValue("");
       return;
     }
     props.onAddGoal(enteredValue);
@@ -32,16 +26,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* !isValid : isValid가 false면 */}
+      {/* invalid 클래스를 추가하고 */}
+      {/* 아니면 클래스 추가 안함 */}
+      <div className={`form-control ${!isValid ? "invalid" : ""} `}>
         <label>목표</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-          style={{
-            backgroundColor: isValid ? "transparent" : "salmon",
-            borderColor: isValid ? "#CCC" : "red",
-          }}
-        />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
