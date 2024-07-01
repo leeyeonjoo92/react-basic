72c1659dda0df4a3600c496783350ff6c420a83e
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-01 13:09:59 +0900

    레포지토리 클론
    48강 리액트 심화 -3- ref

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import NewPayment from './components/NewPayment/NewPayment';
+import React, { useState, Fragment } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
 
   return (
-    <NewPayment/>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
   );
 }
 
