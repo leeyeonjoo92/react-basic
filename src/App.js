312e488d8a6d76e2268cdce034f2b2fae0fc22c6
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-01 13:06:17 +0900

    ReactDOM.createPortal()
    47강 리액트 심화 -2- Portal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import NewPayment from './components/NewPayment/NewPayment';
 
@@ -5,6 +6,30 @@ function App() {
 
   return (
     <NewPayment/>
+=======
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
