658ebcc993e2069eaaba0d05272bf6aa2c9d3049
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-01 14:39:17 +0900

    useRef()
    48강 리액트 심화 -3- ref

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,44 +1,69 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from "react";
 
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import ErrorModal from '../UI/ErrorModal';
-import Wrapper from '../Helpers/Wrapper';
-import classes from './AddUser.module.css';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  // state 사용안함
+  // const [enteredUsername, setEnteredUsername] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
+
+  // useState 대신 useRef를 사용해서 상태관리
+  // value, onChange 필요없음
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    // ref로 지정해준 DOM Node를 딱 찍어줌
+    // console.log(nameInputRef); // {current: input#username}
+    // console.log(ageInputRef); // {current: input#age}
+    // 값을 알고싶으면 current.value
+    // console.log(nameInputRef.current.value); // input에 입력한 이름
+    // console.log(ageInputRef.current.value); // input에 입력한 나이
+
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    // if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
-        title: '유효하지 않은 입력값',
-        message: '유효한 나이와 이름을 입력해주세요 (빈 값이 아닌).',
+        title: "유효하지 않은 입력값",
+        message: "유효한 나이와 이름을 입력해주세요 (빈 값이 아닌).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    // if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
-        title: '유효하지 않은 나이',
-        message: '유효한 나이를 입력해주세요 (> 0).',
+        title: "유효하지 않은 나이",
+        message: "유효한 나이를 입력해주세요 (> 0).",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    // props.onAddUser(enteredUsername, enteredAge);
+    props.onAddUser(enteredName, enteredUserAge);
+    // state 사용 안함
+    // setEnteredUsername("");
+    // setEnteredAge("");
+    // 사용자 추가 후 input 리셋
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
@@ -59,15 +84,17 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // value={enteredUsername}
+            // onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">나이</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">사용자 추가</Button>
         </form>
