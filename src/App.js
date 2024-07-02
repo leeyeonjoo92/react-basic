eb457b223ac1eef342a2b8f6fc5b0c0b9acdc638
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-02 14:16:22 +0900

    레포지토리 클론

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+<<<<<<< Updated upstream
 import './App.css';
 import NewPayment from './components/NewPayment/NewPayment';
 
 function App() {
+=======
+import React, { useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+>>>>>>> Stashed changes
 
   return (
     <NewPayment/>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+<<<<<<< Updated upstream
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +16,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+=======
+root.render(<App />);
+>>>>>>> Stashed changes
