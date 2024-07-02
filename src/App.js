24061851467c17d30e89df61d4bc2c085d6aac53
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-02 16:08:57 +0900

    레포지토리 클론

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import NewPayment from './components/NewPayment/NewPayment';
 
@@ -5,6 +6,26 @@ function App() {
 
   return (
     <NewPayment/>
+=======
+import React, { useContext } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
+
+function App() {
+  const ctx = useContext(AuthContext);
+
+  return (
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
+>>>>>>> Stashed changes
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< Updated upstream
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+=======
+
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './store/auth-context';
+
+>>>>>>> Stashed changes
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
