8823e4fdad7f62c99a03b0464e5a3f41079016fe
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-04 15:03:03 +0900

    fetch() & async/await
    60강 HTTP 요청 -2- GET

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-<<<<<<< Updated upstream
-import './App.css';
-import NewPayment from './components/NewPayment/NewPayment';
-
-function App() {
-
-  return (
-    <NewPayment/>
-=======
 import React from 'react';
 import NewsList from './components/NewsList';
 
 function App() {
   return (
     <NewsList />
->>>>>>> Stashed changes
   );
 }
 
