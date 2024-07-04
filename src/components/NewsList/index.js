f5f149db8c01b2f51b54ff8aba3251fff1371ae4
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-04 15:49:16 +0900

    에러처리
    61강 HTTP 요청 -3- 로딩 및 에러처리

--- src/components/NewsList/index.js
+++ src/components/NewsList/index.js
@@ -10,26 +10,32 @@ const NewsList = () => {
   }, []);
 
   const getNewsList = async () => {
-    // 로딩중 : 백엔드의 응답을 기다리는중
-    setIsLoading(true);
+    try {
+      // 로딩중 : 백엔드의 응답을 기다리는중
+      setIsLoading(true);
 
-    // fetch() : url 주소를 그대로 string으로 넣어주면됨
-    const response = await fetch(
-      "https://newsapi.org/v2/everything?q=tesla&from=2024-06-04&sortBy=publishedAt&apiKey=13697b4b347f4b3ba8b2163635620f60"
-    );
+      // fetch() : url 주소를 그대로 string으로 넣어주면됨
+      // 에러처리 확인하고 싶으면 apikey 맨끝에 아무거나 붙여보기
+      const response = await fetch(
+        "https://newsapi.org/v2/everything?q=tesla&from=2024-06-04&sortBy=publishedAt&apiKey=13697b4b347f4b3ba8b2163635620f60"
+      );
 
-    // 데이터가 먼저 로드되고
-    const data = await response.json();
-    // 그 다음에 콘솔이 찍힘
-    console.log(data);
-    setNews(data.articles);
-    setIsLoading(false);
+      // 데이터가 먼저 로드되고
+      const data = await response.json();
+      // 그 다음에 콘솔이 찍힘
+      console.log(data);
+      setNews(data.articles);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
   return (
     <div>
       {/* 로딩중이 아니면 컨텐츠 불러오기 */}
-      {!isLoading && news.map((news) => <NewsItem {...news} />)}
+      {!isLoading &&
+        news.map((news, index) => <NewsItem key={`news-${index}`} {...news} />)}
       {/* 로딩중이면 문구 뜨게 */}
       {isLoading && <p>로딩중입니다.</p>}
     </div>
