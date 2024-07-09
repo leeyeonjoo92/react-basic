45a4a1e3008aad0da83979977e11355865f8deb9
leeyeonjoo92 <leeyeonjoo92@gmail.com>
2024-07-09 14:53:13 +0900

    store에서 여러개의 state 다루기
    72강 Redux - 3
    
    - payload 함께 전달하기
    - store에서 ...state로 변하지 않는 속성값 함께 전달하기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,15 +2,25 @@ import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   // useSelector 훅을 통해 state의 counter값을 가져옴
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
+
   // action과 관련된 값을 인자로 넣어서 보내줌
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
     // 클릭할때마다 action이 reducer로 전달됨
     dispatch({ type: "increment" });
+  };
+
+  const increase10Handler = () => {
+    // amount 라는 값을 담아서 전달해줌
+    // amount -> payload
+    dispatch({ type: "increase", amount: 10 });
   };
 
   const decrementHandler = () => {
@@ -20,9 +30,10 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>토글 카운터</button>
       <button onClick={incrementHandler}>숫자 증가</button>
+      <button onClick={increase10Handler}>숫자 10 증가</button>
       <button onClick={decrementHandler}>숫자 감소</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,42 @@
 
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
   if (action.type === "increment") {
     return {
+      // 속성들이 많아지면 하나하나 입력해줄수 없음
+      // 바뀌는 값들은 작성해주고
+      // 바뀌지 않는 값들은 기존 state값을 그대로 가져옴
+      ...state,
       counter: state.counter + 1,
+      // showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      ...state,
+      counter: state.counter + action.amount,
+      // showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
+      // showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      // 속성을 여러개 사용할때 속성값을 입력하지 않으면
+      // 이외의 속성을 undefined 처리함
+      // counter라는 속성을 같이 가져가지 않으면 counter를 undefined 처리하기 떄문에 원하는대로 동작하지 않음
+      // counter: state.counter,
+      ...state,
+      showCounter: !state.showCounter,
     };
   }
 
